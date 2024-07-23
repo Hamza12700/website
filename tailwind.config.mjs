ce74000ca870350027b8c84fc8694bc9f4054aa1
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-07-23 06:09:37 +0000

    added: custom colors in extend list

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,14 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-		colors: {
-			"darkish": "#090909",
-			"grayish": "#ada9a9",
-			"white": "#ffffff",
-			"dblue": "#6b7280",
-			"ldark": "#131313"
-		}
+		extend: {
+			colors: {
+				"darkish": "#090909",
+				"grayish": "#ada9a9",
+				"dblue": "#6b7280",
+				"ldark": "#131313"
+			}
+		},
 	},
 	plugins: [require('@tailwindcss/typography')],
 }
